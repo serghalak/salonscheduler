b4f1426923676993868ec3651e358be58a582c33
serghalak <serghalak@gmail.com>
2020-04-14 14:46:39 +0300

    first commit multy-module spring-boot project

---
+++ src/test/java/com/salon/salonscheduler/SalonSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.salonscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
