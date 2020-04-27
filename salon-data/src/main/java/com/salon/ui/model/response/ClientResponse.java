6037fdec6b969b216f40039121e96ecef02e87a8
serghalak <serghalak@gmail.com>
2020-04-27 14:54:10 +0300

    add PersonResponse and CustomResponse. closes #20.

---
+++ salon-data/src/main/java/com/salon/ui/model/response/ClientResponse.java
@@ -0,0 +1,4 @@
+package com.salon.ui.model.response;
+
+public class ClientResponse extends PersonResponse {
+}
---
+++ salon-data/src/main/java/com/salon/ui/model/response/PersonResponse.java
@@ -0,0 +1,21 @@
+package com.salon.ui.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonResponse implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
